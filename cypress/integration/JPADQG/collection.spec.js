bb52d21c257d1fb0f51f5b9c5bd73878eed506fd
Matthias Holcombe <tmatthiash@gmail.com>
2021-12-17 12:43:34 -0600

    added more cypress tests

---
+++ cypress/integration/JPADQG/collection.spec.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+
+describe('VarChar Test', () => {
+    it('Visits the website', () => {
+      cy.visit('http://localhost:3000')
+    })
+
+    it('Fills in the column name', () => {
+        cy.get('input[type=text]').first().type('firstName');
+    })
+
+    it('Sets the type to Integer', () => {
+        cy.get('.MuiSelect-select').click();
+        cy.contains('Collection').click();
+    })
+
+    it('Adds to list', () => {
+        cy.contains('Add To List').click();
+    })
+
+    it('Sets the query type to is/equals', () => {
+        cy.get('.MuiSelect-select').eq(1).click();
+        cy.contains('Is In').click();
+    })
+
+    it('Should have the correct query in the query area', () => {
+        cy.contains('findByFirstNameIn(Collection<firstName> firstName)').should('exist');
+    })
+  })
---
+++ cypress/integration/JPADQG/date.spec.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+
+describe('VarChar Test', () => {
+    it('Visits the website', () => {
+      cy.visit('http://localhost:3000')
+    })
+
+    it('Fills in the column name', () => {
+        cy.get('input[type=text]').first().type('firstName');
+    })
+
+    it('Sets the type to Integer', () => {
+        cy.get('.MuiSelect-select').click();
+        cy.contains('Date').click();
+    })
+
+    it('Adds to list', () => {
+        cy.contains('Add To List').click();
+    })
+
+    it('Sets the query type to is/equals', () => {
+        cy.get('.MuiSelect-select').eq(1).click();
+        cy.contains('Is Between').click();
+    })
+
+    it('Should have the correct query in the query area', () => {
+        cy.contains('findByFirstNameBetween(Date startFirstName, Date endFirstName)').should('exist');
+    })
+  })
